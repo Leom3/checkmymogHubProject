ff1b0b6c53733ad910130547593707014da6ef64
lmaignan <leo.maignan@epitech.eu>
2019-01-23 19:11:57 +0100

    ADD: get location data from web page

--- wowItemDB/python_scraper/get_datas.py
+++ wowItemDB/python_scraper/get_datas.py
@@ -7,8 +7,68 @@ import sys
 import os
 import json
 
+expansion_dic = {
+	7 : 'Battle For Azeroth',
+	6 : 'Legion',
+	5 : 'Warlords Of Draenor',
+	4 : 'Mists Of Pandaria',
+	3 : 'Cataclysm',
+	2 : 'Wrath Of The Lich King',
+	1 : 'The Burning Crusade'
+}
+
+territory_dic = {
+	1 : 'Alliance',
+	2 : 'Horde',
+	0 : 'War Zone'
+}
+
+category_dic = {
+	3 : 'Raid',
+	2 : 'Dungeon',
+	15 : 'Outdoor Zone'
+}
+
+########################################################
+#fonction qui récupère la donnée json dans la page HTML#
+#2 types de regex possibles :                          #
+#  - classification.*location.*name.*count.*outof\:\d+ #
+#  - \"boss\"\:\d+.*name.*count.*outof\:\d+            #
+########################################################
+
+
+def find_json_data_in_file(page):
+	data = re.search('\"boss\"\:\d+[\s\'\,\"classificationlocation\d+id\:\[\]maxlevel\w\-]+', page)
+	if data:
+		json_str = data.group(0)
+		return json_str
+	else:
+		data = re.findall('\{\"classification\"\:\d+[\s\,\"location\d+id\:\[\]maxlevel\w\-]+', page)
+		if data:
+			json_str = data[-1]
+			return json_str
+		else:
+			return -1
+
+def get_location(locate_id, location_json):
+	locate_str = ""
+	for keys in location_json:
+		if (int(keys["id"]) == int(locate_id)):
+			if (keys.get("expansion") != None) and keys["expansion"] in expansion_dic:
+				locate_str = locate_str + expansion_dic[keys["expansion"]]
+			if (keys.get("territory") != None) and keys["territory"] in territory_dic:
+				locate_str = locate_str + ', ' +territory_dic[keys["territory"]]
+			if (keys.get("category") != None) and keys["category"] in category_dic:
+				locate_str = locate_str + ', ' +category_dic[keys["category"]]
+			locate_str = locate_str + " : " + keys["name"]
+			return locate_str
+
+#####################################################################
+#fonction qui boucle sur le fichier d'url pour récuperer les données#
+#####################################################################
+
 def loop_over_files(location_json):
-	print (location_json)
+	#print (location_json)
 	for files in os.listdir("../links"):
 		filename = "../links/" + files
 		with open(filename) as f:
@@ -17,9 +77,15 @@ def loop_over_files(location_json):
 				page_response = requests.get(item, timeout=10)
 				page_content = BeautifulSoup(page_response.content, "html.parser")
 				name = page_content.find("h1", {"class" : "heading-size-1"}).getText()
-				print(name)
-				while 1:
-					i = 0 + 1
+				json_string = find_json_data_in_file(page_content.prettify())
+				if (json_string == -1):
+					print("No source found ! :(")
+					continue
+				else:
+					locate_id = re.search('location\"\:\[\d+\]', json_string)
+					locate_id = ((locate_id.group(0)).replace('location":[', '')).replace(']', '')
+					location_str = get_location(locate_id, location_json)
+					print (location_str)
 
 def create_location_tab():
 	locations = {}
