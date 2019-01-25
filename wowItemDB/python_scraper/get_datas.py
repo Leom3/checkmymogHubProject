42a50f228a2e91e00b3981788d2aba9b8584b299
lmaignan <leo.maignan@epitech.eu>
2019-01-25 12:32:17 +0100

    ADD: Drop rate and better location

--- wowItemDB/python_scraper/get_datas.py
+++ wowItemDB/python_scraper/get_datas.py
@@ -14,7 +14,8 @@ expansion_dic = {
 	4 : 'Mists Of Pandaria',
 	3 : 'Cataclysm',
 	2 : 'Wrath Of The Lich King',
-	1 : 'The Burning Crusade'
+	1 : 'The Burning Crusade',
+	0 : 'Classic'
 }
 
 territory_dic = {
@@ -31,40 +32,69 @@ category_dic = {
 
 ########################################################
 #fonction qui récupère la donnée json dans la page HTML#
-#2 types de regex possibles :                          #
+#3 types de regex possibles :                          #
 #  - classification.*location.*name.*count.*outof\:\d+ #
 #  - \"boss\"\:\d+.*name.*count.*outof\:\d+            #
 ########################################################
 
-
 def find_json_data_in_file(page):
-	data = re.search('\"boss\"\:\d+[\s\'\,\"classificationlocation\d+id\:\[\]maxlevel\w\-]+', page)
+	data = re.search('\"boss\"\:\d+[\s\'\,\"\\\/classificationlocation\d+id\:\[\]maxlevel\w\-\-\_]+', page)
 	if data:
 		json_str = data.group(0)
 		return json_str
 	else:
-		data = re.findall('\{\"classification\"\:\d+[\s\,\"location\d+id\:\[\]maxlevel\w\-]+', page)
+		data = re.findall('\{\"classification\"\:\d+[\s\,\"\\\/location\d+id\:\[\]maxlevel\w\-\'\-\_]+', page)
 		if data:
 			json_str = data[-1]
 			return json_str
 		else:
-			return -1
+			data = re.findall('azerazer', page)
+			if data:
+				json_str = data[-1]
+				return json_str
+			else:
+				return -1
 
-def get_location(locate_id, location_json):
+###########################################################
+# Fonction qui va récuperer l'expansion, le territoire,   #
+# la categorie de localisation et son nom                 #
+#	en remplissant l'objet data_object                    #
+###########################################################
+
+def get_location(locate_id, location_json, object_dic):
 	locate_str = ""
 	for keys in location_json:
 		if (int(keys["id"]) == int(locate_id)):
 			if (keys.get("expansion") != None) and keys["expansion"] in expansion_dic:
-				locate_str = locate_str + expansion_dic[keys["expansion"]]
+				object_dic['expansion'] = expansion_dic[keys["expansion"]]
 			if (keys.get("territory") != None) and keys["territory"] in territory_dic:
-				locate_str = locate_str + ', ' +territory_dic[keys["territory"]]
+				object_dic['territory'] = territory_dic[keys["territory"]]
 			if (keys.get("category") != None) and keys["category"] in category_dic:
-				locate_str = locate_str + ', ' +category_dic[keys["category"]]
-			locate_str = locate_str + " : " + keys["name"]
-			return locate_str
+				object_dic['category_territory'] = category_dic[keys["category"]]
+			object_dic['location'] = keys["name"]
+			return object_dic
+
+###########################################################
+#			calculate the loot rate of an itaem			  #
+#			Thanks to the count number of wowhead         #
+###########################################################
+
+def calculate_loot_rate(loot_rate):
+	value_tab = loot_rate.split(":")
+	value1 = value_tab[0]
+	value2 = value_tab[1]
+	if (value1 == 0 or value2 == 0):
+		loot_rate = 0
+		return loot_rate
+	else:
+		loot_rate = (int(value1) / int(value2)) * 100
+		loot_rate = round(loot_rate, 2)
+		return loot_rate
 
 #####################################################################
 #fonction qui boucle sur le fichier d'url pour récuperer les données#
+# et les remplir dans un objet. Nous utiliserons alors cet objet    #
+# pour écrire dans le fichier servant de base de donnée LUA         #
 #####################################################################
 
 def loop_over_files(location_json):
@@ -74,18 +104,49 @@ def loop_over_files(location_json):
 		with open(filename) as f:
 			content = f.read().splitlines()
 			for item in content:
-				page_response = requests.get(item, timeout=10)
+				object_dic = {
+					'name' : '',
+					'location' : '',
+					'source' : '',
+					'territory' : '',
+					'category_territory' : '',
+					'expansion' : '',
+					'drop_rate' : ''
+				}
+				page_response = requests.get("https://www.wowhead.com/item=159097/paku-blessed-greatbow", timeout=10)
 				page_content = BeautifulSoup(page_response.content, "html.parser")
+				print (page_content)
 				name = page_content.find("h1", {"class" : "heading-size-1"}).getText()
+				object_dic['name'] = name
 				json_string = find_json_data_in_file(page_content.prettify())
 				if (json_string == -1):
-					print("No source found ! :(")
+					print("No source found for " + object_dic['name'])
 					continue
 				else:
-					locate_id = re.search('location\"\:\[\d+\]', json_string)
-					locate_id = ((locate_id.group(0)).replace('location":[', '')).replace(']', '')
-					location_str = get_location(locate_id, location_json)
-					print (location_str)
+					#print (json_string)
+					locate_id = re.search('location\"\:\[[\d\,]+\]', json_string)
+					if (locate_id):
+						locate_id = ((locate_id.group(0)).replace('location":[', '')).replace(']', '')
+						if (locate_id.find(",") != -1):
+							locate_id = locate_id[0:locate_id.find(',')]
+					object_dic = get_location(locate_id, location_json, object_dic)
+					source_name = re.search('name\"\:\"[\\\/\w\,\s\'\-\_]+\"', json_string)
+					source_name = ((((source_name.group(0)).replace('name', '')).replace('\'', '')).replace('":"', '')).replace('"', '')
+					object_dic['source'] = source_name
+					loot_rate = re.search('count\:\d+\,outof\:\d+', json_string)
+					if (loot_rate):
+						loot_rate = ((loot_rate.group(0)).replace('count:', '')).replace(',outof', '')
+					else:
+						loot_rate = 'Unknown'
+					loot_rate = calculate_loot_rate(loot_rate)
+					object_dic["drop_rate"] = loot_rate
+					print (object_dic)
+
+####################################################################################
+#  Fonction qui va créer le tableau de localisation basé sur la BDD wowhead        #
+#  Et en faire un objet JSON que nous utiliserons pour récupérer le nom de la zone #
+#  Le type de territoire ainsi que l'expansion de sortie                           #
+####################################################################################
 
 def create_location_tab():
 	locations = {}
